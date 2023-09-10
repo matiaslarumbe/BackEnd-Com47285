dd7b26e30a1efe7f45c9899e77f279211582d343
Matias Larumbe <matias_larumbe@hotmail.com>
2023-09-09 22:45:30 -0300

    desafio-5-mongoose

---
+++ desafio-5-Mongoose/src/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoose from "mongoose";
+import { engine } from "express-handlebars";
+import { __dirname } from './path.js'
+import { Server } from 'socket.io'
+import path from 'path'
+
+import userRouter from "./routes/users.routes.js";
+import productRouter from "./routes/products.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import messageRouter from "./routes/messages.routes.js";
+
+
+const app = express()
+const PORT = 4000
+
+mongoose.connect('mongodb+srv://matiaslarumbe:coderhouse321@cluster0.anbxejn.mongodb.net/?retryWrites=true&w=majority')
+.then(() => console.log('BDD conectada'))
+.catch(() => console.log('Error en conexion a la BDD'))
+
+app.use(express.json())
+
+app.use('/api/users', userRouter)
+app.use('/api/products', productRouter)
+app.use('api/carts', cartRouter)
+app.use('api/message', messageRouter)
+
+app.listen(PORT, () => {
+    console.log(`Serven on Port ${PORT}`)
+})
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+ //Handlebars
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')  //Sentings de handlebars
+app.set('views', path.resolve(__dirname, './views')) //Ruta de vistas
+
+//socket
+const io = new Server()
+const prods = []
+
+io.on('connection', (socket) => {
+    console.log("Servidor Socket.io conectado")
+    socket.on('mensajeConexion', (info) => {
+        console.log(info)
+    })
+    socket.on('nuevoProducto', (nuevoProd) => {
+        prods.push(nuevoProd)
+        socket.emit('prods', prods)
+    })
+
+})
---
+++ desafio-5-Mongoose/src/routes/cart.routes.js
@@ -0,0 +1,53 @@
+import { Router } from "express";
+import {cartModel} from "../models/carts.models.js"
+import { productModel } from "../models/products.models.js";
+
+
+const cartRouter = Router()
+
+cartRouter.get('/:id', async(req, res) => {
+    const {id} = req.params
+
+    try{
+        const cart = await cartModel.findById(id)
+        if(cart)
+            res.status(200).send({respuesta: 'OK', mensaje: prod})
+        else
+            res.status(404).send({respuesta: 'Error en consultar carrito', mensaje: 'Not found'})
+    } catch(error) {
+        res.status(400).send({respuesta: 'Error en consultar carrito', mensaje: error})
+    }   
+})
+
+cartRouter.post('/:cid/products/:pid', async (req, res) => {
+   const {cid, pid} = req.params
+   const {quantity} = req.body
+
+    try{
+
+        const cart = await cartModel.findById(cid)
+        if(cart) {
+            const prod = await productModel.findById(pid)
+            if (prod) {
+                const indice = cart.products.findIndex(prod => prod.id_prod === pid)
+                if (indice != -1) {
+                    cart.products[indice].quantity = quantity
+                } else {
+                    cart.products.push({id_prod: pid, quantity: quantity})
+                }
+                const respuesta = await cartModel.findByIdAndUpdate(cid, cart)
+                res.status(200).send({respuesta: 'OK', mensaje: respuesta})
+            } else{
+                res.status(404).send({respuesta: 'Error en agregar carrito', mensaje: 'Product Not found'})
+            }
+        } else {
+            res.status(404).send({respuesta: 'Error en agregar carrito', mensaje: 'Cart Not found'})
+        }      
+        
+
+    } catch(error) {
+        res.status(400).send({respuesta: 'Error en agregrar producto carrito', mensaje: error})
+    }   
+})
+
+export default cartRouter
