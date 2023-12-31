8b6c9dcfec1ec43768661b4c0f73d4dc7476bf5b
Matias Larumbe <matias_larumbe@hotmail.com>
2023-12-31 13:15:47 -0300

    Creacion de recupero de contraseña

--- desafio-pre-entrega-3-FrontEnd-BackEnd/Backend/src/routes/recovery.routes.js
+++ desafio-pre-entrega-3-FrontEnd-BackEnd/Backend/src/routes/recovery.routes.js
@@ -1,35 +1,56 @@
 import { Router } from "express";
 import { sendRecoveryMail } from "../config/nodemailer.js";
 import jwt from 'jsonwebtoken';
+import Recovery from "../models/recovery.models.js"; 
 
 const recoveryRouter = Router();
 
-recoveryRouter.post('/password-recovery', (req, res) => {
+
+
+recoveryRouter.post('/password-recovery', async (req, res) => {
     const { email } = req.body;
+    console.log(`Buscando usuario con correo electrónico: ${email}`);
 
     try {
-        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' }); // Generar token JWT
+        const user = await Recovery.findOne({ email });
 
+        if (!user) {
+            console.log(`Usuario con correo electrónico ${email} no encontrado`);
+            return res.status(404).send('Usuario no encontrado');
+        }
+
+        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
         const recoveryLink = `http://localhost:4000/api/recovery/reset-password/${token}`;
 
         sendRecoveryMail(email, recoveryLink);
 
         res.status(200).send('Correo de recuperación enviado');
     } catch (error) {
+        console.log(`Error al buscar usuario: ${error.message}`);
         res.status(500).send(`Error al enviar el mail ${error}`);
     }
 });
 
-recoveryRouter.post('/reset-password/:token', (req, res) => {
+
+
+recoveryRouter.post('/reset-password/:token', async (req, res) => {
     const { token } = req.params;
     const { newPassword, newPassword2 } = req.body;
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verificar y decodificar token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const { email } = decoded;
 
         if (newPassword === newPassword2) {
-            // Aquí puedes actualizar la contraseña del usuario usando el email
+            const user = await recoveryModel.findOne({ email });
+
+            if (!user) {
+                return res.status(404).send('Usuario no encontrado');
+            }
+
+            user.password = newPassword; 
+            await user.save();
+
             res.status(200).send('Contraseña modificada correctamente');
         } else {
             res.status(400).send('Las contraseñas deben ser idénticas');
